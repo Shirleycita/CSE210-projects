00c50c64b65f6770bcda1949fe9063d4ccaede15
Shirleycita <smvycwh6@hotmail.com>
2024-06-05 23:23:12 -0700

    Final 4 Done

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        Running running = new Running(3.2f,"01 Aug", 20); 
+        activities.Add(running); 
+
+        Swimming swimming = new Swimming(200,"02 May", 30);
+        activities.Add(swimming);
+
+        Cycling cycling = new Cycling(7.5f,"03 Jun", 55);
+        activities.Add(cycling);
+
+        foreach (Activity activity in activities)
+        {
+            System.Console.WriteLine(activity.GetSummary());
+        }
+
+
     }
 }
